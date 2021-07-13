32e731c2bc8eb5a1bd5bdf60aa0cd8574bccba5f
Kyle Garcia <kyle.fire@gmail.com>
2021-07-13 15:53:47 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import MainPage from './pages/MainPage';
+import CreatePost from './pages/CreatePost';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='navbar'>
+        <div className='links'>
+          <a href='/'>Main Page</a>
+          <a href='/createpost'>Create Post</a>
+        </div>
+      </div>
+
+      <Router>
+        <Route path='/' exact render={(props) => <MainPage />} />
+        <Route path='/createpost' render={(props) => <CreatePost />} />
+      </Router>
+    </>
   );
 }
 
