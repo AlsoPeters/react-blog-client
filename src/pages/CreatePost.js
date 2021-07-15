3ce3897d1dcb7deedd857a809b7d479b829f4345
Kyle Garcia <kyle.fire@gmail.com>
2021-07-15 20:34:29 +0900

    added simple front end for posting blogs

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react';
+import '../App.css';
+import Axios from 'axios';
 
 export default function CreatePost() {
-    return (
-        <div className='CreatePost'>
-            <h1>CreatePost</h1>
-        </div>
-    )
+  const [userName, setUserName] = useState('');
+  const [title, setTitle] = useState('');
+  const [text, setText] = useState('');
+
+  const submitPost = () => {
+    Axios.post('http://localhost:3001/api/create', {
+      userName: userName,
+      title: title,
+      text: text,
+    });
+  };
+
+  return (
+    <div className='CreatePost'>
+      <div className='uploadPost'>
+        <label>UserName</label>
+        <input
+          type='text'
+          onChange={(e) => {
+            setUserName(e.target.value);
+          }}
+        />
+        <label>Title</label>
+        <input
+          type='text'
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
+        <label>Post Text</label>
+        <textarea
+          onChange={(e) => {
+            setText(e.target.value);
+          }}
+        />
+
+        <button onClick={submitPost}>Submit Post</button>
+      </div>
+    </div>
+  );
 }
